a587152a95884f3e31e20059aa244a9b68cdb312
kimsowon <sowon836@gmail.com>
2025-10-02 01:07:26 +0900

    React 11일차 과제제출

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import styled from "styled-components";
 
 function App() {
   return (
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,21 @@
+import styled from "styled-components";
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: flex-start;
+  gap: 5px;
+  flex-wrap: nowrap;
+`;
+
 export default function Content({ content }) {
   return (
-    <div className="content-container">
+    <ContentContainer className="ContentContainer">
       <img src={content.img} alt={content.title} />
       <span>모집중</span>
       <div>{content.title}</div>
       <p>{content.subtitle}</p>
-    </div>
+    </ContentContainer>
   );
 }
